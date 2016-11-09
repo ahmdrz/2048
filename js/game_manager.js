6726af72e96631e015991aadb5bfc2d25c3b8c89
ahmdrz <ahmadrezazibaei@hotmail.com>
2016-11-09 18:32:13 +0330

    nothing !

--- js/game_manager.js
+++ js/game_manager.js
@@ -170,7 +170,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 2048) { self.won = true; TelegramGameProxy.shareScore(); }
         } else {
           self.moveTile(tile, positions.farthest);
         }
